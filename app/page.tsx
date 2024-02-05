dcaceccf76ae2bf05934674371a731906a925c53
Parthib DS <parthib22.ds@gmail.com>
2024-02-05 22:39:36 +0530

    finally fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,128 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [list, setList] = useState<
+    { id: number; todo: string; check: boolean }[]
+  >(() => {
+    const storedList = localStorage.getItem("TODO_LIST");
+    return storedList ? JSON.parse(storedList) : {};
+  });
+  // const [checked, setChecked] = useState({});
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  // useEffect(() => {
+  //   setList(() => {
+  //     const storedList = localStorage.getItem("TODO_LIST");
+  //     return storedList ? JSON.parse(storedList) : {};
+  //   });
+  // }, []);
+
+  console.log("list: ", list);
+
+  useEffect(() => {
+    localStorage.setItem("TODO_LIST", JSON.stringify(list));
+  }, [list]);
+
+  // useEffect(() => {
+  //   setChecked(() => {
+  //     const checkedList = JSON.parse(localStorage.getItem("checkStatus"));
+  //     return checkedList || [];
+  //   });
+  // }, []);
+
+  // console.log("checked: ", checked);
+
+  // useEffect(() => {
+  //   localStorage.setItem("checkStatus", JSON.stringify(checked));
+  // }, [checked]);
+
+  const addItem = (item: any) => {
+    if (item.trim() !== "") {
+      setList((prev) => [
+        ...prev,
+        {
+          id: prev.length + 1,
+          todo: item.trim(),
+          check: false,
+        },
+      ]);
+    }
+    console.log(list);
+  };
+  const handleKey = (e: any) => {
+    if (e.keyCode === 13) {
+      addItem(e.target.value);
+      e.target.value = "";
+    }
+  };
+  const delItem = (index: any) => {
+    const updatedList = list.filter((_, k) => k !== index);
+    setList(updatedList);
+    console.log(list);
+    // setChecked((prevChecked) => {
+    //   const newChecked = { ...prevChecked };
+    //   delete newChecked[index];
+    //   return newChecked;
+    // });
+  };
+
+  const isChecked = (index: any) => {
+    setList((prev) =>
+      prev.map((item, i) =>
+        i == index ? { ...item, check: !item.check } : item
+      )
+    );
+    // setChecked((prevChecked) => ({
+    //   ...prevChecked,
+    //   [index]: !prevChecked[index],
+    // }));
+  };
+
+  return (
+    <div className="todo">
+      <div>
+        <h1 className="todoTitle">Todo List</h1>
+        <input
+          placeholder="write a task and press enter..."
+          className="todoInput"
+          id="ip"
+          onKeyDown={handleKey}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+      {/* <button
+        onClick={addItem}
+        className="todoAddBtn"
+      >
+        Add
+      </button> */}
+      <h2>
+        {list.length > 0
+          ? list.length + (list.length == 1 ? " task:" : " tasks:")
+          : "No tasks pending."}
+      </h2>
+      <ul>
+        {list.map((item, i) => (
+          <li className="todoList" key={i}>
+            <input
+              className="checkStatus"
+              type="checkbox"
+              checked={item.check || false}
+              // onClick={() => isChecked(i)}
+              onChange={() => isChecked(i)}
+            />
+            {/* <p>{checked ? "ischecked":"notchecked"}</p> */}
+            <span
+              className={item.check ? "strike" : ""}
+              onClick={() => isChecked(i)}
+            >
+              {item.todo}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <button onClick={() => delItem(i)} className="todoDelBtn">
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
